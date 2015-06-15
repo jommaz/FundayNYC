0f8c654947532e16d793d92c4acf938ff0eb59ad
Mazen Jomaa <mjomaa@gmail.com>
2015-06-15 22:06:56 +0000

    Trying to get EventBrite & NYTimes Events API

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'eventbrite'
 gem 'nytimes-events'
 gem 'geocoder'
 
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/funday_controller.rb
+++ app/controllers/funday_controller.rb
@@ -1,7 +1,13 @@
 class FundayController < ApplicationController
+  
+  Chiketto.api_key = CWPKSWECLGL2WOLAJT
+  
+ 
   def new
+    @event = Chiketto::Event.find(123456)
   end
 
   def show
+    
   end
 end
