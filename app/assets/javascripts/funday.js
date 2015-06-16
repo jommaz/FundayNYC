938afc242f01fbf0b0018482e776ff70eda9a24f
Mazen Jomaa <mjomaa@gmail.com>
2015-06-16 21:15:40 +0000

    completed landing page and form

--- app/assets/javascripts/funday.js
+++ app/assets/javascripts/funday.js
@@ -1,4 +1,8 @@
-$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
+
+  $(document).ready(function(){
+     $('.collapsible').collapsible();
+        $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+     });
+    });
