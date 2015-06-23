d64cefbee09e951f20f803f4862c0c822c7c4e21
Mazen Jomaa <mjomaa@gmail.com>
2015-06-23 21:29:14 +0000

    fixed eventbrite logic

--- app/assets/javascripts/funday.js
+++ app/assets/javascripts/funday.js
@@ -1,12 +1,18 @@
-
-  $(document).ready(function(){
-     $('.collapsible').collapsible();
-        $('.datepicker').pickadate({
-        format: 'dddd, dd mmm, yyyy',
-        formatSubmit: "yyyy-mm-dd",
-        hiddenName: true,
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year
-        closeOnSelect: true
-     });
-    });
+  // $(document).ready(function(){
+  //   $('.collapsible').collapsible();
+  //       $('.datepicker').pickadate({
+  //       closeOnSelect: true,
+  //       format: 'dddd, dd mmm, yyyy',
+  //       formatSubmit: "yyyy-mm-dd",
+  //       hiddenName: true,
+  //       selectMonths: true, // Creates a dropdown to control month
+  //       selectYears: 15 // Creates a dropdown of 15 years to control year
+  //   });
+  //   });
+$(document).ready(function(){
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year
+    closeOnSelect: true
+  })
+});
