bd9a59973e18e09aceefc4dd02888a3299a2c366
Mazen Jomaa <mjomaa@gmail.com>
2015-06-26 21:01:21 +0000

    stylings fixed for mobile screen

--- app/assets/javascripts/funday.js
+++ app/assets/javascripts/funday.js
@@ -10,19 +10,31 @@
   //   });
   //   });
 $(document).ready(function(){
-  $('.datepicker').pickadate({
+   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year
-    closeOnSelect: true
-  })
+    min: true,
+    onOpen: function () {
+      this.clear();
+    },
+    onSet: function () {
+      var x,y,year,date,month;
+      x = $('.datepicker').pickadate().val().toString();
+      y = x.split(/[ ,]+/);
+      date = y[0];
+      month = y[1];
+      year = y[2];
+      console.log(y[0]+" "+ y[1]+ " "+ y[2]);
+      if(date && month && year){
+        this.close();
+      }
+    }
+  });
 });
 
 
 $(document).ready(function(){
-   $(".button-collapse").sideNav();
    $('.parallax').parallax();
-   $('.fixed-action-btn').openFAB();
-   $('.fixed-action-btn').closeFAB();
   });
   
    
--- app/assets/javascripts/segment.js
+++ app/assets/javascripts/segment.js
@@ -2,4 +2,6 @@ $(document).ready(function(){
   $("#ref_butn").click(function(){
  location.reload();
   });
+    $('.fixed-action-btn').openFAB();
+  $('.fixed-action-btn').closeFAB();
 })
